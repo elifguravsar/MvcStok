ef1d677ae7ac68aa36d595c74d031ebfb1408d6a
elif güravşar <eliferiha@hotmail.com>
2025-03-02 02:42:58 +0300

    Proje dosyası ekle.

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Guncelle(int id)
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            return View("Guncelle", musteri);
+        }
+
+        [HttpPost]
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERİAD = p1.MUSTERİAD;
+            musteri.MUSTERİSOYAD = p1.MUSTERİSOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
